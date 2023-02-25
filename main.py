790586dff9f854ea22d3a97a976b8a1345cf7115
Moral Siani <MoralSiani@users.noreply.github.com>
2023-02-25 10:57:38 +0200

    decompression

--- main.py
+++ main.py
@@ -8,19 +8,41 @@ import fft
 dtypes = [np.uint8, np.int16, np.int32, np.float32]
 
 
+def decompress(compressed_file_path, out_file):
+    # get data
+    compressed_data = np.load(compressed_file_path)
+    original_time_domain_size = int(np.real(compressed_data[0]))
+    sampling_rate = int(np.real(compressed_data[1]))
+    dtype = dtypes[int(np.real(compressed_data[2]))]
+    padding = int(np.real(compressed_data[-1]))
+    print('Decompressing...')
+    # run inverted fft
+    padded_freq_domain = np.concatenate((compressed_data[3:-1], [0] * padding))
+    time_domain = np.real(fft.fft(padded_freq_domain, inverse=True)).astype(dtype)
+
+    # resize and save
+    time_domain = time_domain[0:original_time_domain_size]
+    wf.write(out_file, sampling_rate, time_domain)
+    print('Decompress complete')
+
+
 def compress_wav(wav_file, out_file):
+    # get data
     sampling_rate, time_domain = wf.read(wav_file)
-    print(f'original len: {len(time_domain)}')
     original_time_domain_size = len(time_domain)
     dtype_idx = dtypes.index(time_domain.dtype)
     freq_res = sampling_rate / len(time_domain)
+    print('Compressing...')
+    # run fft
     time_domain = fft.power2_round_up(time_domain)
     freq_domain = fft.fft(time_domain)
+
+    # compress and save
     freq_cutoff = get_freq_cutoff(len(freq_domain), freq_res)
     compressed_freq_domain, padding = cut_freq_domain(freq_domain, freq_cutoff)
     output_array = np.concatenate(([original_time_domain_size], [sampling_rate], [dtype_idx], compressed_freq_domain, [padding]))
     np.save(out_file, arr=output_array)
-
+    print('Compress complete')
 
 def cut_freq_domain(freq_domain, freq_cutoff):
     compressed_freq_domain = freq_domain[0:freq_cutoff]
@@ -38,5 +60,10 @@ if __name__ == '__main__':
     path = Path.cwd()/f'compressed_files'/f'{file.stem}'
     path.parent.mkdir(parents=True, exist_ok=True)
     compress_wav(file, path)
-    # inverse_results = np.real(fft.fft(fft_results, inverse=True)).astype(np.int16)
+
+    # Decompress
+    out_path = Path.cwd()/f'wav_files'/f'{file.stem}_modified.wav'
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    decompress(f'{path}.npy', out_path)
+
 
